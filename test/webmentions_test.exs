8360d9f6a7f841c26b5137fdc416e63a2441077f
Christian Kruse <cjk@defunct.ch>
2017-01-13 17:18:36 +0100

    ref: update to new elixir version compatibility

--- test/webmentions_test.exs
+++ test/webmentions_test.exs
@@ -6,7 +6,7 @@ defmodule WebmentionsTest do
 
   test "discovers from a link in header" do
     doc = %HTTPotion.Response{status_code: 200, body: "<html>",
-                              headers: %HTTPotion.Headers{hdrs: [link: "<http://example.org/webmentions>; rel=\"webmention\""]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"link" => "<http://example.org/webmentions>; rel=\"webmention\""}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end] do
       assert Webmentions.discover_endpoint("http://example.org") == {:ok, "http://example.org/webmentions"}
@@ -16,7 +16,7 @@ defmodule WebmentionsTest do
   test "discovers from a link in document" do
     doc = %HTTPotion.Response{status_code: 200,
                               body: "<html><head><link rel=\"webmention\" href=\"http://example.org/webmentions\">",
-                              headers: %HTTPotion.Headers{hdrs: ["content-type": "text/html"]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"content-type" => "text/html"}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end] do
       assert Webmentions.discover_endpoint("http://example.org") == {:ok, "http://example.org/webmentions"}
@@ -35,7 +35,7 @@ defmodule WebmentionsTest do
 <link rel="hub" href="https://switchboard.p3k.io">
 <link rel="self" href="http://example.org">
 """,
-                              headers: %HTTPotion.Headers{hdrs: ["content-type": "text/html"]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"content-type" => "text/html"}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end] do
       assert Webmentions.discover_endpoint("http://example.org") == {:ok, "http://example.org/webmentions"}
@@ -44,7 +44,7 @@ defmodule WebmentionsTest do
 
   test "discovers from a link list in header" do
     doc = %HTTPotion.Response{status_code: 200, body: "<html>",
-                              headers: %HTTPotion.Headers{hdrs: [link: "<http://example.org/webmentions>; rel=\"webmention\", <http://example.org/micropub>; rel=\"micropub\", <http://example.org/login>; rel=\"token_endpoint\", <https://indieauth.com/auth>; rel=\"authorization_endpoint\", <https://switchboard.p3k.io>; rel=\"hub\", <http://example.org>; rel=\"self\""]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"link" => "<http://example.org/webmentions>; rel=\"webmention\", <http://example.org/micropub>; rel=\"micropub\", <http://example.org/login>; rel=\"token_endpoint\", <https://indieauth.com/auth>; rel=\"authorization_endpoint\", <https://switchboard.p3k.io>; rel=\"hub\", <http://example.org>; rel=\"self\""}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end] do
       assert Webmentions.discover_endpoint("http://example.org") == {:ok, "http://example.org/webmentions"}
@@ -53,7 +53,7 @@ defmodule WebmentionsTest do
 
   test "discovers from a link list in header when not first element" do
     doc = %HTTPotion.Response{status_code: 200, body: "<html>",
-                              headers: %HTTPotion.Headers{hdrs: [link: "<http://example.org/micropub>; rel=\"micropub\", <http://example.org/login>; rel=\"token_endpoint\", <http://example.org/webmentions>; rel=\"webmention\", <https://indieauth.com/auth>; rel=\"authorization_endpoint\", <https://switchboard.p3k.io>; rel=\"hub\", <http://example.org>; rel=\"self\""]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"link" => "<http://example.org/micropub>; rel=\"micropub\", <http://example.org/login>; rel=\"token_endpoint\", <http://example.org/webmentions>; rel=\"webmention\", <https://indieauth.com/auth>; rel=\"authorization_endpoint\", <https://switchboard.p3k.io>; rel=\"hub\", <http://example.org>; rel=\"self\""}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end] do
       assert Webmentions.discover_endpoint("http://example.org") == {:ok, "http://example.org/webmentions"}
@@ -62,7 +62,7 @@ defmodule WebmentionsTest do
 
   test "correctly sends a mention to example.org/webmentions" do
     doc = %HTTPotion.Response{status_code: 200, body: "<html class=\"h-entry\"><a href=\"http://example.org/test\">blah</a>",
-                              headers: %HTTPotion.Headers{hdrs: [link: "<http://example.org/webmentions>; rel=\"webmention\""]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"link" => "<http://example.org/webmentions>; rel=\"webmention\""}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end,
                          post: fn(_url, _opts) -> doc end] do
@@ -73,7 +73,7 @@ defmodule WebmentionsTest do
 
   test "doesn't send a webmention to a rel=nofollow" do
     doc = %HTTPotion.Response{status_code: 200, body: "<html class=\"h-entry\"><a rel=\"nofollow\" href=\"http://example.org/test\">blah</a>",
-                              headers: %HTTPotion.Headers{hdrs: [link: "<http://example.org/webmentions>; rel=\"webmention\""]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"link" => "<http://example.org/webmentions>; rel=\"webmention\""}}}
 
     with_mock HTTPotion, [get: fn(_url, _opts) -> doc end,
                          post: fn(_url, _opts) -> doc end] do
@@ -83,7 +83,7 @@ defmodule WebmentionsTest do
 
   test "successfully sends mentions to relative URLs" do
     doc = %HTTPotion.Response{status_code: 200, body: "<html class=\"h-entry\"><a href=\"/test\">blah</a>",
-                              headers: %HTTPotion.Headers{hdrs: [link: "<http://example.org/webmentions>; rel=\"webmention\""]}}
+                              headers: %HTTPotion.Headers{hdrs: %{"link" => "<http://example.org/webmentions>; rel=\"webmention\""}}}
 
     with_mock HTTPotion, [get: fn(url, _opts) ->
                            assert url != "/test"
