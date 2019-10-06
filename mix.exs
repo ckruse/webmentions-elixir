685467901e69db76d4ecbbb23cc31570e7a23f15
Christian Kruse <cjk@defunct.ch>
2019-10-06 16:49:28 +0200

    version 0.3.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Webmentions.Mixfile do
   def project do
     [
       app: :webmentions,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
