229c16bdb689448f2f58f4c534d130c5f1c89e69
Christian Kruse <cjk@defunct.ch>
2021-02-20 11:42:55 +0100

    fix: applications => extra_applications do to changes in application boundary checking

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Webmentions.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   def description do
