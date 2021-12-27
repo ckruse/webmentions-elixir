93793493f8279364154d921bd098115404b5c1c6
Christian Kruse <cjk@defunct.ch>
2021-12-27 07:33:13 +0100

    drop support for Elixir < 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Webmentions.Mixfile do
     [
       app: :webmentions,
       version: "1.0.3",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
